f11e52e0fa4a5e9152f8a177e574b75d79fffdd0
Yao Shunyu <yaoshunyu_work@foxmail.com>
2025-04-28 10:16:38 +0800

    终于学会了cmake，跑通了exe

---
+++ build/CMakeFiles/hw1.dir/src/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/hw1.dir/src/main.c.obj: C:\Users\lenovo\Desktop\hw1\src\main.c \
+ C:/MinGW/x86_64-w64-mingw32/include/stdio.h \
+ C:/MinGW/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/MinGW/x86_64-w64-mingw32/include/corecrt.h \
+ C:/MinGW/x86_64-w64-mingw32/include/_mingw.h \
+ C:/MinGW/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/MinGW/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/MinGW/x86_64-w64-mingw32/include/vadefs.h \
+ C:/MinGW/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/MinGW/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/MinGW/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/MinGW/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Users/lenovo/Desktop/hw1/inc/algebra.h
